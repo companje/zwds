25063f2337b2180ced2785e27754ab701466da52
Rick Companje <rick@companje.nl>
2022-07-20 21:56:23 +0200

    update

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 let app = require('express')();
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
+let connections = [];
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
@@ -8,13 +9,51 @@ app.get('/', (req, res) => {
 
 
 http.listen(3000, () => {
-    console.log('Listening on port http://localhost:3000');
+    console.log('Listening on port *: 3000');
 });
 
 io.on('connection', (socket) => {
+    
+    console.log('new connection');
+
+    // connections++;
+    // socket.broadcast.emit('connections', connections);
 
     socket.on('disconnect', () => {
         console.log("A user disconnected");
     });
-    
+
+    socket.on('chat-message', (data) => {
+
+        socket.broadcast.emit('chat-message', (data));
+    });
+
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', (data));
+    });
+
+    socket.on('stopTyping', () => {
+        socket.broadcast.emit('stopTyping');
+    });
+
+    socket.on('joined', (data) => {
+        socket.broadcast.emit('joined', (data));
+
+        connections.push(data); 
+
+        console.log('join',connections,data)
+
+        socket.broadcast.emit('connections', connections.length);
+    });
+
+    socket.on('leave', (data) => {
+        socket.broadcast.emit('leave', (data));
+
+        connections = connections.filter((c)=>c!=data)
+
+        console.log('leave',connections,data)
+
+        socket.broadcast.emit('connections', connections.length);
+    });
+
 });
